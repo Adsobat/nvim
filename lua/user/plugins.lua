99bf76420ca80813e29ad58caa8671167f44d13b
Julian <julian.schmid@exxeta.com>
2024-04-15 09:41:08 +0200

    add git sign

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -256,70 +256,84 @@ local plugins = {
     end,
   },
 
- {
-   "lukas-reineke/indent-blankline.nvim",
---   event = "LazyFile",
-   opts = {
-     indent = {
-       char = "│",
-       tab_char = "│",
-     },
-     scope = { enabled = false },
-     exclude = {
-       filetypes = {
-         "help",
-         "alpha",
-         "dashboard",
-         "neo-tree",
-         "Trouble",
-         "trouble",
-         "lazy",
-         "mason",
-         "notify",
-         "toggleterm",
-         "lazyterm",
-       },
-     },
-   },
-   main = "ibl",
- },
-
-        {
-          "echasnovski/mini.indentscope",
-          version = false, -- wait till new 0.7.0 release to put it back on semver
-          opts = {
-            -- symbol = "▏",
-            symbol = "│",
-            options = { try_as_border = true },
-          },
-          init = function()
-            vim.api.nvim_create_autocmd("FileType", {
-              pattern = {
-                "help",
-                "alpha",
-                "dashboard",
-                "neo-tree",
-                "Trouble",
-                "trouble",
-                "lazy",
-                "mason",
-                "notify",
-                "toggleterm",
-                "lazyterm",
-              },
-              callback = function()
-                vim.b.miniindentscope_disable = true
-              end,
-            })
-          end,
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    --   event = "LazyFile",
+    opts = {
+      indent = {
+        char = "│",
+        tab_char = "│",
+      },
+      scope = { enabled = false },
+      exclude = {
+        filetypes = {
+          "help",
+          "alpha",
+          "dashboard",
+          "neo-tree",
+          "Trouble",
+          "trouble",
+          "lazy",
+          "mason",
+          "notify",
+          "toggleterm",
+          "lazyterm",
         },
-      }
+      },
+    },
+    main = "ibl",
+  },
+
+  {
+    "echasnovski/mini.indentscope",
+    version = false, -- wait till new 0.7.0 release to put it back on semver
+    opts = {
+      -- symbol = "▏",
+      symbol = "│",
+      options = { try_as_border = true },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+          "help",
+          "alpha",
+          "dashboard",
+          "neo-tree",
+          "Trouble",
+          "trouble",
+          "lazy",
+          "mason",
+          "notify",
+          "toggleterm",
+          "lazyterm",
+        },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+    end,
+  },
+
+  -- TREE SITTER
+  {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+  },
+  {
+    "HiPhish/rainbow-delimiters.nvim",
+    dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    },
+  },
+  -- GIt settings
+  "lewis6991/gitsigns.nvim",
+}
 
 
 
 
 
 
-      -- this has to be last
-      require("lazy").setup(plugins)
-      require("luasnip.loaders.from_vscode").lazy_load()-- we need this for friendly-snippets
+-- this has to be last
+require("lazy").setup(plugins)
+require("luasnip.loaders.from_vscode").lazy_load()-- we need this for friendly-snippets

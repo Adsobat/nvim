8264f98190cd969602068c0fe178da0dedacc156
Julian <julian.schmid@exxeta.com>
2025-02-13 10:13:31 +0100

    dissable format on save

--- lua/kickstart/plugins/cmp.lua
+++ lua/kickstart/plugins/cmp.lua
@@ -148,10 +148,10 @@ return {
             --vim_item.kind = string.format('%s', kind_icons[vim_item.kind])
             vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
             vim_item.menu = ({
-              nvim_lsp = '[LSP]',
-              luasnip = '[Snippet]',
-              buffer = '[Buffer]',
               path = '[Path]',
+              nvim_lsp = '[LSP]',
+              buffer = '[Buffer]',
+              luasnip = '[Snippet]',
             })[entry.source.name]
             return vim_item
           end,
